878a8cc4864678c6a2cfa3a088ec37a006310832
Andrew de Oliveira <andrewolip@gmail.com>
2017-11-05 19:24:15 -0200

    Criando operacao de Instaciacao

--- src/main/java/com/andy/cursomc/CursomcApplication.java
+++ src/main/java/com/andy/cursomc/CursomcApplication.java
@@ -1,12 +1,29 @@
 package com.andy.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.andy.cursomc.domain.Categoria;
+import com.andy.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		repo.save(cat1);
+		repo.save(cat2);
 	}
 }
