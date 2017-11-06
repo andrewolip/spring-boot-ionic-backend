a7a2db2df480b5050ae28ed54990fe36e0552f33
Andrew de Oliveira <andrewolip@gmail.com>
2017-11-06 20:18:46 -0200

    Estado e Cidade

---
+++ src/main/java/com/andy/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.andy.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andy.cursomc.domain.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
---
+++ src/main/java/com/andy/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.andy.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andy.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
